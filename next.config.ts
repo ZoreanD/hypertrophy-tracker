c96067585e0cf65f8fb6e709139a0e775f0cfcb3
ZoreanD <aems112008@hotmail.com>
2026-06-03 22:33:35 -0500

    fix: remove unsupported skipWaiting from next-pwa config

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,6 @@ const withPWAConfig = withPWA({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
-  skipWaiting: true,
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
